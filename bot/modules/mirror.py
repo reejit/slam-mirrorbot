067abe59cdbf0ba8a1b229bcdd213c7293d8e8b6
reejit <70316178+reejit@users.noreply.github.com>
2021-03-07 21:09:08 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -139,7 +139,7 @@ class MirrorListener(listeners.MirrorListeners):
 
     def onUploadComplete(self, link: str, size):
         with download_dict_lock:
-            msg = f'<b>Filename 📂: </b><code>{download_dict[self.uid].name()}</code>\n<b>🔶Size🔶: </b><code>[{size}]</code>'
+            msg = f'<b>Filename 📂: </b><code>{download_dict[self.uid].name()}</code>\n\n<b>🔶Size🔶: </b><code>[{size}]</code>'
             buttons = button_build.ButtonMaker()
             if SHORTENER is not None and SHORTENER_API is not None:
                 surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, link)).text
@@ -167,7 +167,7 @@ class MirrorListener(listeners.MirrorListeners):
             else:
                 uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
             if uname is not None:
-                msg += f'\n\<b>©️ COPYRIGHT ©️</b> : {uname}'
+                msg += f'<b>👥Share Url:👥</b>: <code>{share_url}</code>\n\n<b>©️ COPYRIGHT ©️</b> : {uname}'
             try:
                 fs_utils.clean_download(download_dict[self.uid].path())
             except FileNotFoundError:
